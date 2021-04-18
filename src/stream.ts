53913f7594e064a678aa5adeafa80875815bb134
strautins <strautins.janis@gmail.com>
2021-04-18 20:50:13 +0300

    Title processing

--- src/stream.ts
+++ src/stream.ts
@@ -6,14 +6,25 @@ export interface IAudioStream {
 }
 
 export const streamFactory = (cb: (data: string) => void) => {
-  const st = spawn("mpg123", [configManager.getStreamUrl()]);
-
-  st.stdout.on("data", (data) => {
-    cb(data);
+  const st = spawn("mpg123", [configManager.getStreamUrl()], {
+    stdio: "pipe",
   });
+
+  const listen = (data: string) => {
+    if (data.indexOf("StreamTitle") >= 0) {
+      const titleSegment = data.split(";")[0];
+      const sIndex = titleSegment.indexOf("'");
+      const lIndex = titleSegment.indexOf("'");
+
+      cb(titleSegment.substring(sIndex, lIndex));
+    }
+  };
+
+  process.stdout.on("data", listen);
 
   return {
     close: () => {
+      process.stdout.removeListener("data", listen);
       st.kill();
     },
   };
